fcea50b61b0854b8f4533f8878299c152ded9d7f
ventuscoder <shivankcode@gmail.com>
2023-03-10 17:22:48 +0530

    Load data onto page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [msg, setMsg] = useState('')
+
+  useEffect(()=>{
+    async function getData() {
+      let res = await fetch('http://localhost:8000/');
+      let data = await res.json();
+      setMsg(data.message);
+    }
+    getData();
+  }, [])
 
   return (
     <div className="App">
-      
+      {msg}
     </div>
   )
 }
