eb239bcfe2636fbca9744da3047fec0630b192f1
ventuscoder <shivankcode@gmail.com>
2023-03-13 20:38:27 +0530

    Style the new poll page

--- src/pages/newpollpage.jsx
+++ src/pages/newpollpage.jsx
@@ -10,11 +10,13 @@ function NewPollPage(props) {
     }
 
     return (
-        <div className='new-poll-page'>
-            <input type="text" value={pollId} className='text-copy' readOnly />
-            <button onClick={copyText}>Copy poll code</button>
-            <p>{copyStatus}</p>
-            <p>Don't forget to save this code and share it with your friends so they can vote!</p>
+        <div className='new-poll-page row p-2'>
+            <input type="text" value={pollId} className='rounded text-copy p-2 mt-2 col-sm-4' readOnly />
+            <button onClick={copyText} className='rounded copy-code-btn col-sm-3 mt-2'>Copy poll code</button>
+            <p className='copy-desc'>{copyStatus}</p>
+            <p className='copy-desc'>Don't forget to save this code and share it with your friends so they can vote!</p>
+            <p className='copy-desc'>View your poll yourself by copying the code and pasting it on the form on our homepage.</p>
+            <p className='copy-desc'>Reload the page to get redirected to the homepage</p>
         </div>
     )
 }
